079f12474c9e6a327e54b883fe2d5e785312a361
Cameron Yee-Ping <cameronyeeping@gmail.com>
2024-07-01 15:14:26 -0700

    Added user_queries.h and database_connection.h

---
+++ src/database_connection.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "../include/database_connection.h"
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+   int i;
+   for(i = 0; i<argc; i++) {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
