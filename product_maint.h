3140757d37b02166208088a6b0badc38db863ff3
Ananya Singh <139885386+ananyasingh8@users.noreply.github.com>
2024-07-01 14:18:04 -0700

    Added product_maint.h

---
+++ product_maint.h
@@ -0,0 +1,8 @@
+#ifndef PRODUCT_MAINT_H
+#define PRODUCT_MAINT_H
+#include <string>
+
+void create_product_release(std::string product_name, std::string release_id, std::string release_date);
+void create_product(std::string product_name);
+
+#endif
---
+++ user_maint.h
@@ -0,0 +1,7 @@
+#ifndef USER_MAINT_H
+#define USER_MAINT_H
+#include <string>
+
+void create_user(std::string user_name, std::string phone_number, std::string email, std::string department);
+
+#endif
