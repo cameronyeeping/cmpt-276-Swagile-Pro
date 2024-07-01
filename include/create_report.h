298d2a7864ea9cefd17533a6803e5d05d193d63c
Ananya Singh <139885386+ananyasingh8@users.noreply.github.com>
2024-07-01 15:16:00 -0700

    added comments

--- include/create_report.h
+++ include/create_report.h
@@ -2,7 +2,11 @@
 #define CREATE_REPORT_H
 #include <string>
 
+// Prints all reports with status not done nor cancelled
 void create_report_1();
+
+// Prints a list of customers/staff who need to be informed when a particular 
+// change has been implemented, and in what ProductRelease ID.
 void create_report_2();
 
 #endif
