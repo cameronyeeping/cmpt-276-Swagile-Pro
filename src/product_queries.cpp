3a7d9b4ea2c8b7a0e41abf63bdfd7d886f630cf0
Ananya Singh <139885386+ananyasingh8@users.noreply.github.com>
2024-07-15 18:07:38 -0700

    added add_product_release_query()

--- src/product_queries.cpp
+++ src/product_queries.cpp
@@ -3,3 +3,12 @@
 #include "../include/macros.h"
 using namespace std;
 
+void add_product_release_query(string product_name, string release_id, string release_date)
+{
+    sqlite3_stmt* stmt;
+    int exit = sqlite3_prepare_v2(db, ADD_PRODUCT_RELEASE_QUERY, -1, &stmt, nullptr);
+
+    sqlite3_bind_text(stmt, 1, product_name.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, release_id.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, release_date.c_str(), -1, SQLITE_STATIC);
+}
