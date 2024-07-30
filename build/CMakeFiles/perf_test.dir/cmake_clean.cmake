01926e100e58e62a69ccf81ad8bfbfc72744a5ac
Cameron Yee-Ping <cameronyeeping@gmail.com>
2024-07-30 12:38:46 -0700

    debugging change request

---
+++ build/CMakeFiles/perf_test.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/perf_test.dir/src/create_report.cpp.o"
+  "CMakeFiles/perf_test.dir/src/create_report.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/database_connection.cpp.o"
+  "CMakeFiles/perf_test.dir/src/database_connection.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/globals.cpp.o"
+  "CMakeFiles/perf_test.dir/src/globals.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/issue_manag.cpp.o"
+  "CMakeFiles/perf_test.dir/src/issue_manag.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/issue_queries.cpp.o"
+  "CMakeFiles/perf_test.dir/src/issue_queries.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/perf_test.cpp.o"
+  "CMakeFiles/perf_test.dir/src/perf_test.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/product_maint.cpp.o"
+  "CMakeFiles/perf_test.dir/src/product_maint.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/product_queries.cpp.o"
+  "CMakeFiles/perf_test.dir/src/product_queries.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/report_queries.cpp.o"
+  "CMakeFiles/perf_test.dir/src/report_queries.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/ui.cpp.o"
+  "CMakeFiles/perf_test.dir/src/ui.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/user_maint.cpp.o"
+  "CMakeFiles/perf_test.dir/src/user_maint.cpp.o.d"
+  "CMakeFiles/perf_test.dir/src/user_queries.cpp.o"
+  "CMakeFiles/perf_test.dir/src/user_queries.cpp.o.d"
+  "perf_test"
+  "perf_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/perf_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
